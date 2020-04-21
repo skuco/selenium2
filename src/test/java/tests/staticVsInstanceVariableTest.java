ad027ee2ac508bae4e158b2111a0a73c709f5a25
skuco <Str@toc@ster1>
2020-04-21 18:49:38 +0200

    added test for understanding static variables and classes

---
+++ src/test/java/tests/staticVsInstanceVariableTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import org.junit.*;
+
+public class staticVsInstanceVariableTest {
+    static int staticNumber;
+    int instanceNumber = 0;
+
+    @BeforeClass
+    public static void mainSetUp() {
+        System.out.println("### Main SetUp ###");
+        staticNumber = 0;
+        System.out.println("Static number: " + staticNumber);
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("Test SetUp");
+    }
+
+    @Test
+    public void testA(){
+        System.out.println("Test A");
+        staticNumber++;
+        System.out.println("Static Number: " + staticNumber);
+        instanceNumber++;
+        System.out.println("Instance Number: " + instanceNumber);
+    }
+    @Test
+    public void testB(){
+        System.out.println("Test B");
+        staticNumber = 10;
+        System.out.println("Static Number: " + staticNumber);
+        instanceNumber = 20;
+        System.out.println("Instance Number: " + instanceNumber);
+    }
+    @Test
+    public void testC(){
+        System.out.println("Test C");
+        staticNumber--;
+        System.out.println("Static Number: " + staticNumber);
+        instanceNumber--;
+        System.out.println("Instance Number: " + instanceNumber);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Test Tear Down");
+    }
+
+    @AfterClass
+    public static void mainTearDown() {
+        System.out.println("### Main TearDown ###");
+    }
+
+}
