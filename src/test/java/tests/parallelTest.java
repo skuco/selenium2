0604199fa8d31536d2896a09c56968146a649b86
skuco <Str@toc@ster1>
2020-04-24 17:29:22 +0200

    added tests for @Rule and @Concurrent run

---
+++ src/test/java/tests/parallelTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import com.google.code.tempusfugit.concurrency.ConcurrentTestRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+// Java micro-library for writing & testing concurrent code
+// Home Page: "http://tempusfugitlibrary.org/"
+// Add it to the POM
+
+@RunWith(ConcurrentTestRunner.class)
+public class parallelTest {
+    public WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testA(){
+        System.out.println("Running test A");
+        driver.get("http://dsl.sk/");
+    }
+    @Test
+    public void testB(){
+        System.out.println("Running test B");
+        driver.get("http://dsl.sk/");
+    }
+    @Test
+    public void testC(){
+        System.out.println("Running test C");
+        driver.get("http://dsl.sk/");
+    }
+    @Test
+    public void testD(){
+        System.out.println("Running test D");
+        driver.get("http://dsl.sk/");
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/ruleTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import tools.initialSetup;
+
+public class ruleTest extends initialSetup {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void openBaseUrl(){
+        driver.get(getBASE_URL() + "waitforit.php");
+    }
+
+    @Test
+    public void dummyTestForRule(){
+        expectedException.expect(ComparisonFailure.class);
+        expectedException.expectMessage("Title is wrong!");
+
+        WebElement text = driver.findElement(By.xpath("//div/h1[text()='wait for it !']"));
+        Assert.assertEquals("Title is wrong!" , "wait for it !" , text.getText());
+    }
+
+}
